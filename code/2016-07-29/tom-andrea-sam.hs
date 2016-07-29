4d11787503074e9ecb7d5f153766a4a03f58de5e
tom <tarjohnson1979@gmail.com>
2016-07-29 16:16:45 +0100

    Non-stable version of sorting by length frequencywq

--- code/2016-07-29/tom-andrea-sam.hs
+++ code/2016-07-29/tom-andrea-sam.hs
@@ -1,6 +1,7 @@
 import Data.List
 import Data.Ord
 import GHC.Exts
+import Control.Monad
 
 compareLengths a b
   | lenA < lenB  = LT
@@ -19,3 +20,7 @@ lsort2 = sortBy (\a b -> compare (length a) (length b))
 
 lsort3:: [[a]] -> [[a]]
 lsort3 = sortWith length 
+
+freqSort:: [[a]] -> [[a]]
+freqSort xs = join (groupBy (equal length) (sortWith length xs))
+                where equal f a b = (f a) == (f b)
