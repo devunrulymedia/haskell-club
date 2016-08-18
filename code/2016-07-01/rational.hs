5ec4f87b440e5d2836e4b9fe9dd26988d4828a5b
Alex Cowell <alxcwll@gmail.com>
2016-08-18 12:29:08 +0100

    Fixed compiler warnings

--- code/2016-07-01/rational.hs
+++ code/2016-07-01/rational.hs
@@ -1,13 +1,15 @@
 data Rational' = Rational' Int Int deriving (Show)
 
+
 instance Num Rational' where 
-        Rational' a b * Rational' c d = Rational' (a * c) (b * d)
-        Rational' a b + Rational' c d = Rational' ((a * d) + (c * b))  (b * d)
-	abs (Rational' a b) = Rational' (abs a) (abs b)
-	signum (Rational' a b) = Rational' ((signum a) * (signum b)) 1
-	negate (Rational' a b) = Rational' (negate a) b
-	fromInteger a = Rational' (fromIntegral a) 1
-	
+    Rational' a b * Rational' c d = Rational' (a * c) (b * d)
+    Rational' a b + Rational' c d = Rational' ((a * d) + (c * b))  (b * d)
+    abs (Rational' a b) = Rational' (abs a) (abs b)
+    signum (Rational' a b) = Rational' ((signum a) * (signum b)) 1
+    negate (Rational' a b) = Rational' (negate a) b
+    fromInteger a = Rational' (fromIntegral a) 1
+    
+
 instance Fractional Rational' where
---	(/) (Rational' a b) (Rational' c d) = (Rational' a b) * (Rational' d c)
-	recip (Rational' a b) = (Rational' b a) 
+--  (/) (Rational' a b) (Rational' c d) = (Rational' a b) * (Rational' d c)
+    recip (Rational' a b) = (Rational' b a) 
