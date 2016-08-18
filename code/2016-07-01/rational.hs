9b5564ba66d0fd7e4aee469b692906b9e3657c97
Alex Cowell <alxcwll@gmail.com>
2016-08-18 12:46:23 +0100

    Introduced lowestTerms function for keeping the rationals under control

--- code/2016-07-01/rational.hs
+++ code/2016-07-01/rational.hs
@@ -2,13 +2,20 @@ data Rational' = Rational' Int Int
 
 
 instance Num Rational' where 
-    Rational' a b * Rational' c d = Rational' (a * c) (b * d)
-    Rational' a b + Rational' c d = Rational' ((a * d) + (c * b))  (b * d)
-    abs (Rational' a b) = Rational' (abs a) (abs b)
-    signum (Rational' a b) = Rational' ((signum a) * (signum b)) 1
-    negate (Rational' a b) = Rational' (negate a) b
-    fromInteger a = Rational' (fromIntegral a) 1
+    Rational' a b * Rational' c d = lowestTerms $ Rational' (a * c) (b * d)
+    Rational' a b + Rational' c d = lowestTerms $ Rational' ((a * d) + (c * b))  (b * d)
+    abs (Rational' a b)           = Rational' (abs a) (abs b)
+    signum (Rational' a b)        = Rational' ((signum a) * (signum b)) 1
+    negate (Rational' a b)        = Rational' (negate a) b
+    fromInteger a                 = Rational' (fromIntegral a) 1
     
+
+lowestTerms :: Rational' -> Rational'
+lowestTerms (Rational' a b) = Rational' numerator denominator
+    where greatestCommonDivisor = gcd a b
+          numerator = a `div` greatestCommonDivisor
+          denominator = b `div` greatestCommonDivisor
+
 
 instance Fractional Rational' where
 --  (/) (Rational' a b) (Rational' c d) = (Rational' a b) * (Rational' d c)
