a0e736031f7f7cb6135f5139e7649b853bcba29e
tom <tarjohnson1979@gmail.com>
2016-11-25 16:38:31 +0000

    Add guards to prevent division by 0, or anything which doesn't result in an int. Improve show. Go back to ints for readability

--- code/2016-11-25/countdown.hs
+++ code/2016-11-25/countdown.hs
@@ -1,37 +1,41 @@
 import Data.List
 
-data Solution = Val Rational | Sum Solution Solution | Diff Solution Solution | Mult Solution Solution | Div Solution Solution deriving (Show)
+data Solution = Val Int | Sum Solution Solution | Diff Solution Solution | Mult Solution Solution | Div Solution Solution
 
-eval :: Solution -> Rational
+instance Show Solution where
+  show (Val x) = show x
+  show (Sum x y) = "(" ++ (show x) ++ "+" ++ (show y) ++ ")"
+  show (Mult x y) = "(" ++ (show x) ++ "*" ++ (show y) ++ ")"
+  show (Diff x y) = "(" ++ (show x) ++ "-" ++ (show y) ++ ")"
+  show (Div x y) = "(" ++ (show x) ++ "/" ++ (show y) ++ ")"
+
+eval :: Solution -> Int
 eval (Val a) = a
 eval (Sum a b) = eval a + eval b
 eval (Diff a b) = eval a - eval b		
 eval (Mult a b) = eval a * eval b
-eval (Div a b) = eval a / eval b
+eval (Div a b) = eval a `quot` eval b
 
 solutions :: Solution -> Solution -> [Solution]
-solutions x y = [x, 
-                 y, 
-                 Sum x y, 
-                 Mult x y, 
-                 Diff x y, 
-                 Diff y x, 
-                 Div x y, 
-                 Div y x]
+solutions x y 
+  | eval x == 0 && eval y == 0 = []
+  | eval x == 0 = [y]
+  | eval y == 0 = [x]
+  | rem (eval x) (eval y) == 0 = (Div x y) : usuals
+  | rem (eval y) (eval x) == 0 = (Div y x) : usuals
+  | otherwise = usuals
+  where usuals = [x, y, Sum x y, Mult x y, Diff x y, Diff y x]
 
-solves :: Rational -> Solution -> Bool
+solves :: Int -> Solution -> Bool
 solves t s = eval s == t
-
-solve :: Rational -> Rational -> Rational -> [Solution]
-solve t v1 v2 = filter (solves t) (solutions (Val v1) (Val v2))
 
 allSolutions :: [Solution] -> [Solution]
 allSolutions [] = []
 allSolutions [x] = [x]
 allSolutions (x:xs) = concat (map (solutions x) (allSolutions xs))
 
-solve'' :: Rational -> [Solution] -> [Solution]
+solve'' :: Int -> [Solution] -> [Solution]
 solve'' t xs = filter (solves t) (concat $ map allSolutions (permutations xs))
 
-solve' :: Rational -> [Rational] -> [Solution]
-solve' t vs = solve'' t (map Val vs)
+solve :: Int -> [Int] -> [Solution]
+solve t vs = solve'' t (map Val vs)
