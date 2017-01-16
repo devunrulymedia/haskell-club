3e117df900423b7e745fceb025e6bd91b518ac19
bruceeddy <bruce@bruceeddy.org>
2017-01-16 19:03:35 +0000

    make length actually be a left fold.  add reverse implemented as a right fold.  add initial implementation of take as a left fold

--- code/2017-01-13/bruce_length_take_reverse.hs
+++ code/2017-01-13/bruce_length_take_reverse.hs
@@ -3,8 +3,11 @@ length' :: [a] -> Int
 length' [] = 0
 length' (x:xs) = 1 + length' xs
 
+--length'' :: [a] -> Int
+--length'' = foldr (\_ acc -> acc+1 ) 0
+
 length'' :: [a] -> Int
-length'' = foldr (\_ acc -> acc+1 ) 0
+length'' = foldl (\ acc _ -> acc+1 ) 0
 
 take' :: Int -> [a] -> [a]
 take' 0 _ = []
@@ -13,11 +16,15 @@ take' n (x:xs)
 	| n < 0     = x:xs
 	| otherwise = x:(take' (n-1) xs)
 
+take'' :: Int -> [a] -> [a]
+take'' n l = fst $ (foldl (\ (xs, count) x -> if (count >= n ) then (xs, count+1) else (xs ++ [x], count+1) ) ([], 0)) l
+
 reverse' :: [a] -> [a]
 reverse' [] = []
 reverse' (x:xs) = (reverse' xs) ++ [x]
 
-
+reverse'' :: [a] -> [a]
+reverse'' = foldr (\ x xs -> xs	 ++ [x]) []
 
 
 
