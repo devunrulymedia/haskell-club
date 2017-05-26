0398ceabba57e82ac7c072ea87935ef2cd9fab0f
tomj <tarjohnson1979@gmail.com>
2017-05-26 14:59:28 +0100

    Finished the basic magic square implementation

--- code/Strand Beginners/2017-05-26/magicsquare.hs
+++ code/Strand Beginners/2017-05-26/magicsquare.hs
@@ -1,6 +1,28 @@
+data Result = Empty | Sum Int | Inconsistent deriving Show
+
+isMagic :: [[Int]] -> Bool
+isMagic xs = case foldl magicSoFar Empty (sets xs) of
+  (Sum _) -> True
+  otherwise -> False
+
+magicSoFar :: Result -> [Int] -> Result
+magicSoFar Empty xs = Sum (sum xs)
+magicSoFar Inconsistent xs = Inconsistent
+magicSoFar (Sum count) xs
+  | (sum xs) == count = Sum count
+  | otherwise = Inconsistent
+
+sets :: [[Int]] -> [[Int]]
+sets square = concat $ map ($ square) [rows, columns, diagonals]
+
 rows :: [[Int]] -> [[Int]]
 rows = id
 
 columns :: [[Int]] -> [[Int]]
 columns ([]:rows) = [] 
 columns xs = (map head xs) : (columns (map tail xs))
+
+diagonals :: [[Int]] -> [[Int]]
+diagonals xs = [diagonal' xs, diagonal' $ reverse xs] where
+  diagonal' [] = []
+  diagonal' ((x:xs):ys) = x : diagonal' (map tail ys)
