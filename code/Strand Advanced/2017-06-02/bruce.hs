ef66e8445a4f80faada76f635d63256c4df6b61d
bruceeddy <bruce@bruceeddy.org>
2017-06-09 11:33:12 +0100

    a little documentation

--- code/Strand Advanced/2017-06-02/bruce.hs
+++ code/Strand Advanced/2017-06-02/bruce.hs
@@ -6,3 +6,6 @@ lessThan a b = elem (a,b) [(A 1,A 2),(A 3,A 4)]
 instance Ord A where
 	compare a b | a `lessThan` b = LT
 		    | otherwise = GT
+
+-- now you can do:
+-- Prelude Data.List>sort [A 1, A 5, A 4, A 3]
