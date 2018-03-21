5ece3cd05249165bf08ce8a0b343181c21b900b5
tomj <tarjohnson1979@gmail.com>
2018-03-21 11:59:15 +0000

    Extract asset loading and world creation from Main

--- code/pandamonium/app/Main.hs
+++ code/pandamonium/app/Main.hs
@@ -5,16 +5,10 @@ module Main(
 import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Game
 import Codec.BMP
-import World
-import Block
-import Ball
 import Renderable
 import Updatable
-import GameEvent
-import Shape
-import Paddle
-import Player
-import Controller
+import Assets
+import CreateWorld
 
 window :: Display
 window = InWindow "A window!" (500, 500) (100, 100)
@@ -25,44 +19,7 @@ background = black
 fps :: Int
 fps = 600
 
-scene :: [Block]
-scene = [ Block (Rectangle (-200) 200 100 90) white
-        , Block (Rectangle (-200) 200 (-90) (-100)) white
-        , Block (Rectangle (-5) 5 (-40) (-90)) white ]
-
-p1paddle = Paddle (-150, 0) (Rectangle (-5) 5 25 (-25)) 200 (withKeys (Char 'a') (Char 'z'))
-p2paddle = Paddle (150, 0)  (Rectangle (-5) 5 25 (-25)) 200 (withKeys (Char '\'') (Char '/'))
-
-paddleList = [p1paddle, p2paddle]
-
-playerList :: [Player]
-playerList =
-  [ Player
-    { paddle = p1paddle
-    , score = 0
-    , scoreLocation = (-120, 125)
-    , hue = orange
-    , endzone = Block (Rectangle 190 200 100 (-100)) orange
-    , index = 1
-    }
-  , Player
-    { paddle = p2paddle
-    , score = 0
-    , scoreLocation = (80, 125)
-    , hue = blue
-    , endzone = Block (Rectangle (-200) (-190) 100 (-100)) blue
-    , index = 2
-    }
-  ]
-
 main :: IO ()
-main = do sprite <- loadBMP "resources/sprites/ball.bmp"
-          let initBall = Ball (20, 0) (200, 300) sprite
-          let initialWorld = World
-                              { scenery = scene
-                              , ball = initBall
-                              , initialBall = initBall
-                              , events = []
-                              , players = playerList
-                              }
-          play window background fps initialWorld render listen update
+main = do assets <- loadAssets
+          let world = createWorld assets
+          play window background fps world render listen update
