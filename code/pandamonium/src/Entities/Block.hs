27c812025bcd82696581b8cdadcb11e7ea38e4a0
tomj <tarjohnson1979@gmail.com>
2018-03-21 12:17:40 +0000

    Put the various entities into their own folder

---
+++ code/pandamonium/src/Entities/Block.hs
@@ -0,0 +1,13 @@
+module Entities.Block where
+
+import Shape
+import Graphics.Gloss (Color, color)
+import Renderable
+
+data Block = Block Shape Color deriving (Show, Eq)
+
+instance Shaped Block where
+  shape (Block s _) = s
+
+instance Renderable Block where
+  render (Block s c) = color c $ render s
