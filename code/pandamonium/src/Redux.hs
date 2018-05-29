4f6b946701a52ec15c63cb9f6def8c7fa8da9f25
tom <tarjohnson1979@gmail.com>
2018-05-29 22:31:00 +0100

    Introduce connecting of reduxes (or redii): given a redux on an a, and a lens of a in b, yield a redux of b. Also, introduce composition of multiple redii into a single redux

--- code/pandamonium/src/Redux.hs
+++ code/pandamonium/src/Redux.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE RankNTypes #-}
+
 module Redux where
 
 import Graphics.Gloss.Interface.IO.Game
 import Control.Monad.Writer
+import Control.Lens
 import Data.DList
 
 type Events e w = Writer (DList e) w
@@ -22,9 +25,6 @@ noOpRedux = Redux
   , listener = noOp
   }
 
-compose :: Monad m => [a -> b -> m b] -> a -> b -> m b
-compose fs a b = foldM (\x f -> f a x) b fs
-
 fireEvent :: e -> Events e ()
 fireEvent event = tell $ singleton event
 
@@ -35,3 +35,22 @@ reduxListen r e w = case runWriter $ listener r e w of
 reduxUpdate :: Redux w e -> Float -> w -> IO w
 reduxUpdate r t w = case runWriter $ updater r t w of
   (world, events) -> foldM (flip $ reducer r) world events
+
+lensing :: Functor f => Lens b b a a -> (e -> a -> f a) -> e -> b -> f b
+lensing lens f = \e -> lens %%~ (f e)
+
+connect :: Redux a e -> Lens b b a a -> Redux b e
+connect redux lens = Redux
+  { reducer  = lensing lens (reducer redux)
+  , updater  = lensing lens (updater redux)
+  , listener = lensing lens (listener redux)
+  }
+
+compose :: [Redux w e] -> Redux w e
+compose redii = Redux
+  { reducer  = compose' (reducer <$> redii)
+  , updater  = compose' (updater <$> redii)
+  , listener = compose' (listener <$> redii)
+  } where
+    compose' :: Monad m => [a -> b -> m b] -> a -> b -> m b
+    compose' fs a b = foldM (\x f -> f a x) b fs
