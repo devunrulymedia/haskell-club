80719d5c730482a78e0666c5435ab0a1245edb8d
Tom Johnson <tarjohnson1979@gmail.com>
2018-06-28 19:02:35 +0100

    This is a good application for... an Applicative!

--- code/pandamonium/src/Shapes/Collisions/PolygonCollisions.hs
+++ code/pandamonium/src/Shapes/Collisions/PolygonCollisions.hs
@@ -7,6 +7,7 @@ import Data.Maybe
 import Debug.Trace
 import Shapes.Datatypes
 import Shapes.Movables
+import Control.Applicative
 
 data DePoly = DePoly [ Vector ] [ Vector ] deriving (Show, Eq)
 
@@ -21,38 +22,22 @@ project :: [ Vector ] -> Vector -> (Float, Float)
 project points normal = let projections = (dotV normal) <$> points
                          in (foldl1 min projections, foldl1 max projections)
 
-push :: (Float, Float) -> (Float, Float) -> Maybe Float
-push (min1, max1) (min2, max2)
+pushProj :: (Float, Float) -> (Float, Float) -> Maybe Float
+pushProj (min1, max1) (min2, max2)
   | min1 > max2 = Nothing
   | max1 < min2 = Nothing
-  | otherwise   = let pushLeft = min1 - max2
-                      pushRight = max1 - min2
-                   in if abs pushLeft < abs pushRight
-                 then Just pushLeft
-                 else Just pushRight
+  | otherwise   = Just $ smaller (min1 - max2) (max1 - min2)
+    where smaller a b = if abs a < abs b then a else b
 
-pushes :: DePoly -> DePoly -> [ (Vector, Maybe Float) ]
-pushes (DePoly p1s n1s) (DePoly p2s n2s) = push' p1s p2s <$> (n1s ++ n2s) where
-  push' p1s p2s normal = let proj1 = project p1s normal
-                             proj2 = project p2s normal
-                             pushout = push proj1 proj2
-                          in (normal, pushout)
-
-shorter :: Vector -> Vector -> Vector
-shorter a b = if sqMagV a < sqMagV b then a else b
+push :: [ Vector ] -> [ Vector ] -> Vector -> Maybe Vector
+push p1s p2s normal = let proj1 = project p1s normal
+                          proj2 = project p2s normal
+                          pushDist = pushProj proj1 proj2
+                       in (flip mulSV normal) <$> pushDist
 
 pushOut :: DePoly -> DePoly -> Maybe Vector
-pushOut (DePoly p1s n1s) (DePoly p2s n2s) = pushOut' (n1s ++ n2s) Nothing where
-  pushOut' :: [ Vector ] -> Maybe Vector -> Maybe Vector
-  pushOut' [] shortest = shortest
-  pushOut' (normal : rest) shortest =
-    let proj1 = project p1s normal
-        proj2 = project p2s normal
-     in case (flip mulSV normal) <$> push proj1 proj2 of
-        Nothing        -> Nothing
-        (Just current) -> pushOut' rest $ case shortest of
-          Nothing     -> Just current
-          (Just prev) -> Just $ shorter prev current
+pushOut (DePoly p1s n1s) (DePoly p2s n2s) = foldl1 (liftA2 shorter) $ push p1s p2s <$> (n1s ++ n2s)
+  where shorter a b = if sqMagV a < sqMagV b then a else b
 
 instance Collides DePoly where
   (!!!) a b = isJust $ pushOut a b
