d9ba604e1463c8d089d63dd6caa6f77d163ccc71
Bruce Eddy <github@bruceeddy.org>
2018-07-06 02:40:33 +0100

    represent orientation of thruster as a vector instead of an angle; move the correction for the rotation the bitmap happens to have out to the render logic

--- code/pandamonium/src/World/CreateWorld.hs
+++ code/pandamonium/src/World/CreateWorld.hs
@@ -21,5 +21,5 @@ walls = [ Block (rectangle (-310) 310 230 220) white
 
 createWorld :: Assets -> World
 createWorld assets = World { _scenery = walls
-                           , _thruster = Thruster (0, 0) (0, 0) (0, 0) 0 (withKeys (Char 'z') (Char 'x') (Char 'm')) (shipSprite assets)
+                           , _thruster = Thruster (0, 0) (0, 0) (0, 0) (0, 1) (withKeys (Char 'z') (Char 'x') (Char 'm')) (shipSprite assets)
                            }
