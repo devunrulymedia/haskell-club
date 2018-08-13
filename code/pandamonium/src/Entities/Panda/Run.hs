aa0673da74213c8dc2da8594f57eb8923016ccb9
Tom Johnson <tarjohnson1979@gmail.com>
2018-08-13 19:04:48 +0100

    Clean up run logic using joypad

--- code/pandamonium/src/Entities/Panda/Run.hs
+++ code/pandamonium/src/Entities/Panda/Run.hs
@@ -13,10 +13,10 @@ reverseBoost :: Float
 reverseBoost = 2.5
 
 moveHorizontally :: Float -> Panda -> Panda
-moveHorizontally t pd = let (x, y) = velocity pd in case pd ^. controller of
-  (Controller (ControlState True False _) _) -> applyImpulse (t*(-hv)*(if x > 0 then reverseBoost else 1), 0) $ pd
-  (Controller (ControlState False True _) _) -> applyImpulse (t*hv*(if x < 0 then reverseBoost else 1), 0) $ pd
-  otherwise -> vel %~ (slowBy hv) $ pd where
+moveHorizontally t pd = let (x, y) = velocity pd in case (toJoypad $ pd ^. controller) of
+  (Joypad JLeft _)    -> applyImpulse (t*(-hv)*(if x > 0 then reverseBoost else 1), 0) $ pd
+  (Joypad JRight _)   -> applyImpulse (t*hv*(if x < 0 then reverseBoost else 1), 0) $ pd
+  (Joypad JNeutral _) -> vel %~ (slowBy hv) $ pd where
     slowBy :: Float -> Vector -> Vector
     slowBy f (x, y)
       | x > 0 = (x - (min x (t*f)), y)
