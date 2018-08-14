7d9270f1f7abb83753d11a89a9f136e566ecd64b
Tom Johnson <tarjohnson1979@gmail.com>
2018-08-14 12:56:04 +0100

    jumping animation, part un

--- code/pandamonium/src/Entities/Panda/Animation.hs
+++ code/pandamonium/src/Entities/Panda/Animation.hs
@@ -6,10 +6,20 @@ import Entities.Panda.Panda
 import Entities.Panda.MovementStateMachine
 import Control.Lens
 
+hang_speed :: Float
+hang_speed = 150
+
+jumpingSprite :: Panda -> Int
+jumpingSprite pd = js' (pd ^. vel) where
+  js' (x, y)
+    | y > hang_speed = 7
+    | y < (-hang_speed) = 9
+    | otherwise = 8
+
 spriteFor :: Panda -> Picture
 spriteFor pd = (pd ^. sprites) !! (frame $ pd ^. state) where
   frame Grounded = 1
-  frame Airborne = 7
+  frame Airborne = jumpingSprite pd
   frame (WallHugging _) = 2
 
 faceRightDirection :: Panda -> Picture -> Picture
