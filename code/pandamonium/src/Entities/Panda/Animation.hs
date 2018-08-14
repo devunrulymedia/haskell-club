2a5cd1abd3d4097e79db989b0fcb3c4b194ec639
Tom Johnson <tarjohnson1979@gmail.com>
2018-08-14 09:02:49 +0100

    First steps towards animation: display different frames based on the state

--- code/pandamonium/src/Entities/Panda/Animation.hs
+++ code/pandamonium/src/Entities/Panda/Animation.hs
@@ -3,7 +3,14 @@ module Entities.Panda.Animation where
 import Graphics.Gloss
 import Renderable
 import Entities.Panda.Panda
+import Entities.Panda.MovementStateMachine
 import Control.Lens
 
+spriteFor :: Panda -> Picture
+spriteFor pd = (pd ^. sprites) !! (frame $ pd ^. state) where
+  frame Grounded = 1
+  frame Airborne = 7
+  frame (WallHugging _) = 2
+
 instance Renderable Panda where
-  render pd = let (x, y) = pd ^. pos in translate x y $ scale 2 2 $ (pd ^. sprites) !! 0
+  render pd = let (x, y) = pd ^. pos in translate x y $ scale 2 2 $ spriteFor pd
