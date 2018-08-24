50344da310694f549e8120031f7158855cbafa7e
Tom Johnson <tarjohnson1979@gmail.com>
2018-08-24 09:10:10 +0100

    Allow switching between games in main

--- code/pandamonium/app/Main.hs
+++ code/pandamonium/app/Main.hs
@@ -4,11 +4,19 @@ module Main(
 
 import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Game
+import Pandamonium.Renderable
+import Pandamonium.Redux
+import Pandamonium.World.Assets
+import Pandamonium.World.CreateWorld
+import Pandamonium.Game.Game
+import Pandamonium.World.Stages.Stage1
+import Pandamonium.World.Stages.Stage2
 import Thrust.Renderable
 import Thrust.Redux
 import Thrust.World.Assets
 import Thrust.World.CreateWorld
 import Thrust.Game.Game
+
 
 window :: Display
 window = FullScreen
@@ -19,16 +27,20 @@ background = black
 fps :: Int
 fps = 600
 
+-- main :: IO ()
+-- main = thrust
+
 main :: IO ()
-main = thrust
+main = pandamonium
+
 
 thrust :: IO ()
-thrust = do assets <- loadAssets
-            let world = createWorld assets
-            let game = withWorld world
-            playIO window background fps game iorender (reduxListen gameRedux) (reduxUpdate gameRedux)
+thrust = do assets <- Thrust.World.Assets.loadAssets
+            let world = Thrust.World.CreateWorld.createWorld assets
+            let game = Thrust.Game.Game.withWorld world
+            playIO window background fps game Thrust.Renderable.iorender (Thrust.Redux.reduxListen Thrust.Game.Game.gameRedux) (Thrust.Redux.reduxUpdate Thrust.Game.Game.gameRedux)
 
--- pandamonium :: IO ()
--- pandamonium = do assets <- loadAssets
---           let game = withStages assets (cycle [stage2, stage1])
---           playIO window background fps game iorender (reduxListen gameRedux) (reduxUpdate gameRedux)
+pandamonium :: IO ()
+pandamonium = do assets <- Pandamonium.World.Assets.loadAssets
+                 let game = Pandamonium.Game.Game.withStages assets (cycle [stage2, stage1])
+                 playIO window background fps game Pandamonium.Renderable.iorender (Pandamonium.Redux.reduxListen Pandamonium.Game.Game.gameRedux) (Pandamonium.Redux.reduxUpdate Pandamonium.Game.Game.gameRedux)
