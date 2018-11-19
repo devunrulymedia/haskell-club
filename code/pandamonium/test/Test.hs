8ea0d40b062605f828f0acd703188482351ac14a
Tom Johnson <tarjohnson1979@gmail.com>
2018-11-19 18:39:25 +0000

    Start building bombs, including an align-to-grid function

--- code/pandamonium/test/Test.hs
+++ code/pandamonium/test/Test.hs
@@ -9,5 +9,6 @@ import {-@ HTF_TESTS @-} Shapes.PolygonCollisions
 import {-@ HTF_TESTS @-} Redux.ReduxTest
 import {-@ HTF_TESTS @-} Redux.TimerTest
 import {-@ HTF_TESTS @-} Components.EntityTest
+import {-@ HTF_TESTS @-} Bomberman.BombTest
 
 main = htfMain htf_importedTests
