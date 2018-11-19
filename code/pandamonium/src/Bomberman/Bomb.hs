0397fa7d787ad246ded2526c0a44cc3697c72354
Tom Johnson <tarjohnson1979@gmail.com>
2018-11-19 18:46:57 +0000

    Rename inner function to align

--- code/pandamonium/src/Bomberman/Bomb.hs
+++ code/pandamonium/src/Bomberman/Bomb.hs
@@ -11,9 +11,9 @@ data DropBomb = DropBomb Owner Float Float deriving ReduxEvent
 data Owner = Owner EntityId deriving Component
 
 alignToGrid :: (Float, Float) -> (Float, Float) -> (Float, Float) -> (Float, Float)
-alignToGrid (cx, cy) (sx, sy) (px, py) = (slot cx sx px, slot cy sy py) where
-  slot :: Float -> Float -> Float -> Float
-  slot c s p = (fromIntegral (round ((p - c) / s))) * s + c
+alignToGrid (cx, cy) (sx, sy) (px, py) = (align cx sx px, align cy sy py) where
+  align :: Float -> Float -> Float -> Float
+  align c s p = (fromIntegral (round ((p - c) / s))) * s + c
 
 bomb :: EntityId -> Float -> Float -> Entity
 bomb owner x y = entity
