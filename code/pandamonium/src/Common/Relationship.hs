afcd36386bb667574fcb23e9d6f3b7c615c550a7
Tom Johnson <tarjohnson1979@gmail.com>
2018-11-20 18:45:44 +0000

    Express relationship in terms of relationshipM'

--- code/pandamonium/src/Common/Relationship.hs
+++ code/pandamonium/src/Common/Relationship.hs
@@ -21,17 +21,13 @@ relationshipM :: Monad m
              -> Lens c c a a
              -> Lens c c b b
              -> i -> c -> m c
-relationshipM f lensA lensB i c = relationshipM' (f i) lensA lensB c
+relationshipM f lensA lensB i = relationshipM' (f i) lensA lensB
 
 relationship :: (i -> a -> b -> (a, b))
              -> Lens c c a a
              -> Lens c c b b
              -> i -> c -> c
-relationship f lensA lensB i c =
-  let a = c ^. lensA
-      b = c ^. lensB
-      (a', b') = f i a b
-   in lensA .~ a' $ lensB .~ b' $ c
+relationship f lensA lensB i c = runIdentity $ relationshipM' (\a b -> return $ f i a b) lensA lensB c
 
 onList :: Monad m => (a -> b -> m (a, b)) -> a -> [b] -> m (a, [b])
 onList f a [] = return (a, [])
